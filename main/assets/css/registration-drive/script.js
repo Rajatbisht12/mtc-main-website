92243b368369f96b499a99f580f368c8685ffd1c
Vansh Raj Chauhan <Vanshchauhan1310@gmail.com>
2023-08-28 23:12:52 +0530

    website updated

--- main/assets/css/registration-drive/script.js
+++
@@ -1,7 +0,0 @@
-const regBtn = document.querySelector(".button button");
-
-const url = "https://forms.gle/s29hdhjhmrApG2mj7"; // add google form link here
-
-regBtn.addEventListener("click", () => {
-  window.open(url, "_blank");
-});
